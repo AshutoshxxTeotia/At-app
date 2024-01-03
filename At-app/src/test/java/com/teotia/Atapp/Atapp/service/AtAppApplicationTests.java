875ebc49587b0ff6d84d5c285e4341b2e49d9375
Ashutosh <ashutosh10teotia@gmail.com>
2024-01-03 13:36:14 +0530

    initial draft

---
+++ At-app/src/test/java/com/teotia/Atapp/Atapp/service/AtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teotia.Atapp.Atapp.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
